9809d1629f9dfdd5dc1d93632d3c26c12ac225cb
Kanwal <alikanwal382@gmail.com>
2024-06-07 01:01:44 -0700

    update hint button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { FaKickstarterK, FaSadCry, FaSmile } from "react-icons/fa";
+import { FaSadCry, FaSmile } from "react-icons/fa";
 
 export default function Home() {
   const [randomNum, setRandomNum] = useState<any>(0);
@@ -8,7 +8,15 @@ export default function Home() {
   const [userGuess, setUserGuess] = useState(""); // New state for storing user's guess
   const [click, setClick] = useState(false);
 
+  //hint state
+  const [hint,setHint]=useState<any>("")
+  const [hintClick,setHintClick]=useState<boolean>(false)
+  const[showHint,setShowHint]=useState<boolean>(false)
+
+
+
   const handler = () => {
+    setShowHint(false)
     setUserGuess(inputValue); // Store the user's guess before clearing the input
     setClick(true);
     setRandomNum(Math.floor(Math.random() * 10 + 1));
@@ -23,6 +31,26 @@ export default function Home() {
     }
   };
 
+
+
+ //hint fnction
+  const hintHandler= () =>{
+    setShowHint(true)
+   setHintClick(true)
+    if (randomNum >=1 && randomNum <=4) {
+     setHint( "Guessing number is between 1 and 4")
+    } else if (randomNum >=5 && randomNum <=7) {
+      setHint("Guessing number is between 5 and 7")
+    } else if (randomNum >=8 && randomNum <=10) {
+      setHint("Guessing number is between 8 and 10")
+    }
+    
+  }
+
+
+
+
+
   return (
     <main className="text-center mt-8">
       <h1 className="text-3xl font-bold text-yellow-600">
@@ -36,13 +64,33 @@ export default function Home() {
         className="mt-8 p-4  rounded-lg border-2 border-yellow-600 w-full max-w-md focus:text-green-900"
       />
       <div className="m-8">
-        <button
+         && <button
           onClick={handler}
           className="text-white m-2 bg-yellow-600 p-4 rounded-lg pl-8 pr-8 font-bold text-xl"
         >
           Result
         </button>
-        {click && userGuess != "" && (
+
+
+    
+
+       {/* hint button */}
+        {!showHint && <button
+          onClick={hintHandler}
+          className="text-white m-2 bg-yellow-600 p-4 rounded-lg pl-8 pr-8 font-bold text-xl"
+        >
+          Get Hint
+        </button>}
+
+
+        {hintClick && showHint && <div className=" bg-yellow-600 text-black"><p>{hint}</p></div>}
+
+
+
+
+
+
+        {click && userGuess != "" && !showHint &&(
           <div className="text-white mt-4">
             <p className="text-yellow-600 font-bold text-2xl">
               Your guessing number: {userGuess} {/* Display the user's guess */}
@@ -73,3 +121,9 @@ export default function Home() {
     </main>
   );
 }
+
+
+
+
+
+
